9999d39a830e72fef0927d23eee137f7e6a8ce02
Wolfgang Fahl <wf@bitplan.com>
2019-09-17 16:36:18 +0200

    fixes #2

--- gt1.py
+++ gt1.py
@@ -1,4 +1,7 @@
+# minimal imports
 from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+# get the remote graph traversal
 g = traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin','g'))
-print(g.V().count().toList())
+vCount=g.V().count().next()
+print("The modern graph has %d vertices" % (vCount))
