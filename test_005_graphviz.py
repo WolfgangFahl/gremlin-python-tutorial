0c552fb5abcdca50a0547e20ad2933e6cc4add18
Wolfgang Fahl <wf@bitplan.com>
2019-09-25 08:56:19 +0200

    improves graphviz

--- test_005_graphviz.py
+++ test_005_graphviz.py
@@ -1,8 +1,8 @@
-# see https://github.com/WolfgangFahl/gremlin-python-tutorial/blob/master/test_001.py
+# see https://github.com/WolfgangFahl/gremlin-python-tutorial/blob/master/test_005_graphviz.py
 from tutorial import remote
 from graphviz import Digraph
 import os.path
-
+from gremlin_python.process.traversal import T
 
 # initialize a remote traversal
 g = remote.RemoteTraversal().g()
@@ -12,13 +12,22 @@ def test_createGraphvizGraph():
     remoteTraversal=remote.RemoteTraversal()
     remoteTraversal.load("tinkerpop-modern.xml")
     dot = Digraph(comment='Modern')
-    for v in g.V().toList():
-        # quirky way to get properties see https://github.com/nedlowe/gremlin-python-example/blob/master/app.py
-        vProps=g.V(v.id).valueMap().next()
-        #print (vProps)
-        dot.node("node%d" % (v.id),vProps["name"][0])
+    # get vertice properties including id and label
+    for vDict in g.V().valueMap(True).toList():
+        # print vDict
+        vId=vDict[T.id]
+        vLabel=vDict[T.label]
+        gvLabel="%s\n%s\nname=%s" % (vId,vLabel,vDict["name"][0])
+        if "age" in vDict:
+            gvLabel=gvLabel+"\nage=%s" % (vDict["age"][0])
+        dot.node("node%d" % (vId),gvLabel)
     for e in g.E():
-        dot.edge("node%d" % (e.inV.id),"node%d" % (e.outV.id))
+        eDict=g.E(e.id).valueMap(True).next()
+        print (e,eDict)
+        geLabel="%s\n%s\nweight=%s" % (e.id,e.label,eDict["weight"])
+        dot.edge("node%d" % (e.outV.id),"node%d" % (e.inV.id),label=geLabel)
+    dot.edge_attr.update(arrowsize='2',penwidth='2')
+    dot.node_attr.update(style='filled',fillcolor="#A8D0E4")
     print (dot.source)
     dot.render('/tmp/modern.gv', view=False)
     assert os.path.isfile('/tmp/modern.gv.pdf')
