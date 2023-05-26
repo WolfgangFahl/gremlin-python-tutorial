a72f86f0a8f18e8b5d003273d3963aee074ed8c5
Wolfgang Fahl <wf@bitplan.com>
2023-05-26 17:19:30 +0300

    refactors

--- tests/test_003_connection.py
+++ tests/test_003_connection.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 # see https://github.com/apache/tinkerpop/blob/master/gremlin-python/src/main/jython/tests/driver/test_client.py
-from gremlin_python.process.anonymous_traversal import traversal
-from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.driver.request import RequestMessage
-from gremlin import gremote
+from gremlin.remote import RemoteTraversal
+from tests.basetest import Basetest
 
-# test a connection
-def test_connection():
-    # see https://github.com/apache/tinkerpop/blob/master/gremlin-python/src/main/jython/gremlin_python/driver/driver_remote_connection.py
-    remoteTraversal=gremote.RemoteTraversal()
-    g = remoteTraversal.g()
-    t=g.V()
-    remoteConnection=remoteTraversal.remoteConnection
-    # see https://github.com/apache/tinkerpop/blob/master/gremlin-python/src/main/jython/gremlin_python/driver/client.py
-    client=remoteConnection._client
+class TestConnection(Basetest):
+    """
+    test connection handling
+    """
+    # test a connection
+    def test_connection(self):
+        # see https://github.com/apache/tinkerpop/blob/master/gremlin-python/src/main/jython/gremlin_python/driver/driver_remote_connection.py
+        remoteTraversal=RemoteTraversal()
+        g = remoteTraversal.g()
+        t=g.V()
+        remoteConnection=remoteTraversal.remoteConnection
+        # see https://github.com/apache/tinkerpop/blob/master/gremlin-python/src/main/jython/gremlin_python/driver/client.py
+        client=remoteConnection._client
+    
+        connection=client._get_connection()
+        message = RequestMessage('traversal', 'bytecode', {'gremlin': t.bytecode, 'aliases': {'g': client._traversal_source}})
+        results_set = connection.write(message).result()
+        future = results_set.all()
+        results = future.result()
+        print ("%d results" % (len(results)))
+        #assert len(results) == 6
+        assert isinstance(results, list)
+        assert results_set.done.done()
+        #assert 'host' in results_set.status_attributes
+        print (results_set.status_attributes)
 
-    connection=client._get_connection()
-    message = RequestMessage('traversal', 'bytecode', {'gremlin': t.bytecode, 'aliases': {'g': client._traversal_source}})
-    results_set = connection.write(message).result()
-    future = results_set.all()
-    results = future.result()
-    print ("%d results" % (len(results)))
-    #assert len(results) == 6
-    assert isinstance(results, list)
-    assert results_set.done.done()
-    #assert 'host' in results_set.status_attributes
-    print (results_set.status_attributes)
 
-test_connection()
--- tests/test_004_io.py
+++ tests/test_004_io.py
@@ -1,32 +1,34 @@
-from gremlin import gremote
 import os
+from tests.base_gremlin_test import BaseGremlinTest
+class TestIo(BaseGremlinTest):
+    """
+    test Io handling
+    """
+    
+    # test loading a graph
+    def test_loadGraph(self):
+        g=self.g
+        graphmlFile="../data/air-routes-small.xml";
+        # make the local file accessible to the server
+        airRoutesPath=os.path.abspath(graphmlFile)
+        # drop the existing content of the graph
+        g.V().drop().iterate()
+        # read the content from the air routes example
+        g.io(airRoutesPath).read().iterate()
+        vCount=g.V().count().next()
+        print (f"{graphmlFile} has {vCount} vertices")
+        assert vCount==47
+    
+    # test saving a graph
+    def test_saveGraph(self):
+        g=self.g
+        graphmlPath="/tmp/A-Fish-Named-Wanda.xml"
+        # drop the existing content of the graph
+        g.V().drop().iterate()
+        g.addV("Fish").property("name","Wanda").iterate()
+        g.io(graphmlPath).write().iterate()
+        print(f"wrote graph to {graphmlPath}")
+        # check that the graphml file exists
+        assert os.path.isfile(graphmlPath)
 
-# initialize a gremote traversal
-g = gremote.RemoteTraversal().g()
 
-# test loading a graph
-def test_loadGraph():
-   graphmlFile="air-routes-small.xml";
-   # make the local file accessible to the server
-   airRoutesPath=os.path.abspath(graphmlFile)
-   # drop the existing content of the graph
-   g.V().drop().iterate()
-   # read the content from the air routes example
-   g.io(airRoutesPath).read().iterate()
-   vCount=g.V().count().next()
-   print ("%s has %d vertices" % (graphmlFile,vCount))
-   assert vCount==47
-
-# test saving a graph
-def test_saveGraph():
-   graphmlPath="/tmp/A-Fish-Named-Wanda.xml"
-   # drop the existing content of the graph
-   g.V().drop().iterate()
-   g.addV("Fish").property("name","Wanda").iterate()
-   g.io(graphmlPath).write().iterate()
-   print("wrote graph to %s" % (graphmlPath))
-   # check that the graphml file exists
-   assert os.path.isfile(graphmlPath)
-
-test_loadGraph()
-test_saveGraph()
